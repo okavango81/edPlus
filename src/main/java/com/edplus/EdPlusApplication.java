ba3ddc282b482c2d357d4bdf89572bcf7dcca9bd
okavango81 <eguilhermeleitedev81@gmail.com>
2024-03-28 14:51:06 -0300

    created project

---
+++ src/main/java/com/edplus/EdPlusApplication.java
@@ -0,0 +1,13 @@
+package com.edplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edplus/EdPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
